af00c286fe4de8cd500e4d9a26a38735467ab47d
kitety <aizaizuori@foxmail.com>
2018-09-01 11:12:16 +0800

    增加判断函数

--- src/index.js
+++ src/index.js
@@ -19,148 +19,157 @@ import './index.css'
 //转换为功能组件
 //props 为 this.props
 function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    )
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 class Board extends React.Component {
-    // 取出数组里面的值
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
+  // 取出数组里面的值
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            history: [
-                { squares: Array(9).fill(null) }
-            ],
-            stepNumber: 0,
-            xIsNext: true
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [
+        { squares: Array(9).fill(null) }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    }
+  }
+  handleClick(i) {
+    // 复制数组,而且固定了复制的长度
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    //当前的
+    const current = history[history.length - 1]
+    // 复制数组,都是在前面的基础上复制
+    const squares = current.squares.slice();
+    // 已经胜利或者已经选择就return
+    if (calculateWinner(squares)) {
+      return;
+    }
+    if (squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // 这个已经是在操作this.state了,每次次改都要重置
+    //这里要用xIsNext: !this.state.xIsNext,因为前面可以读到this.state,后面的呢不可以
+    this.setState({
+      // concat 返回新的数组,抛弃了固定了复制的长度的之前的
+      history: history.concat([
+        {
+          squares: squares
         }
-    }
-    handleClick(i) {
-        // 复制数组,而且固定了复制的长度
-        const history = this.state.history.slice(0, this.state.stepNumber + 1)
-        //当前的
-        const current = history[history.length - 1]
-        // 复制数组,都是在前面的基础上复制
-        const squares = current.squares.slice();
-        // 已经胜利或者已经选择就return
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        // 这个已经是在操作this.state了,每次次改都要重置
-        //这里要用xIsNext: !this.state.xIsNext,因为前面可以读到this.state,后面的呢不可以
-        this.setState({
-            // concat 返回新的数组,抛弃了固定了复制的长度的之前的
-            history: history.concat([
-                {
-                    squares: squares
-                }
-            ]),
-            stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
-        })
-    }
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0
-        })
-    }
-    render() {
-        const history = this.state.history
-        const current = history[this.state.stepNumber]
-        const winner = calculateWinner(current.squares)
-        const moves = history.map((step, move) => {
-            // step遍历的数组 move为索引
-            const desc = move ? 'Go to move #' + move : 'Go to game start'
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            )
-        })
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    })
+    setTimeout(() => {
+      if (!!calculateWinner(squares)) {
+        alert('win')
+        return;
+      }
+    }, 100);
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    const winner = calculateWinner(current.squares)
+    const moves = history.map((step, move) => {
+      // step遍历的数组 move为索引
+      const desc = move ? 'Go to move #' + move : 'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
 
-        let status;
-        if (winner) {
-            status = `Winner ${winner}`
-        } else {
-            status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O')
-        }
-        return (
-            // 传递的是数组
-            <div className="game">
-                <div className="game-board">
-                    <Board
-                        squares={current.squares}
-                        onClick={(i) => { this.handleClick(i) }}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
-                </div>
-            </div>
-        );
+    let status;
+    if (winner) {
+      status = `Winner ${winner}`
+    } else {
+      status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O')
     }
+    return (
+      // 传递的是数组
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => { this.handleClick(i) }}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 
 ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  <Game />,
+  document.getElementById('root')
 );
 function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-    for (let i = 0; i < lines.length; i++) {
-        // 解构赋值
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    // 解构赋值
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[i];
     }
-    return null;
+  }
+  return false;
 }
